4bf1da926fca844bd85b9a2bf46d9fd954575eba
yryuu <yryuuyamazaki@gmail.com>
2018-07-09 21:08:35 +0900

    todo save action定義

--- src/app/store-todo/todo.actions.ts
+++ src/app/store-todo/todo.actions.ts
@@ -1,11 +1,27 @@
 import { Action } from '@ngrx/store';
+import { Todo } from '../util/model/todo';
 
 export enum TodoActionTypes {
-  LoadTodos = '[Todo] Load Todos'
+  /** Todoリストの取得 **/
+  LoadTodos = '[Todo] Load Todos',
+  /** Todoを保存する */
+  SaveTodo = '[Todo] Save Todo',
+  /** private Todo保存成功時 */
+  SaveTodoSuccess = '[Todo] Save Todo Success',
 }
 
 export class LoadTodos implements Action {
   readonly type = TodoActionTypes.LoadTodos;
 }
 
-export type TodoActions = LoadTodos;
+export class SaveTodo implements Action {
+  readonly type = TodoActionTypes.SaveTodo;
+  constructor(public payload: { todo: Todo }) {
+  }
+}
+
+export class SaveTodoSuccess implements Action {
+  readonly type = TodoActionTypes.SaveTodoSuccess;
+}
+
+export type TodoActions = LoadTodos | SaveTodo | SaveTodoSuccess;
